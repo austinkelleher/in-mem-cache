657cef541b62c4de6c12c18792fb97e959f0fbfb
Charlie Duong <charlieduong94@gmail.com>
2017-02-13 11:00:54 -0500

    Add option to selectively emit events when entries are put or deleted

--- CacheEntry.js
+++ CacheEntry.js
@@ -8,6 +8,14 @@ class CacheEntry {
     this.ttl = ttl;
     this.expiresTimestamp = (ttl === -1) ? undefined : (Date.now() + ttl);
     this.keepAliveOnAccess = (options.keepAliveOnAccess !== false);
+
+    // apply cache options value as default if entry option is
+    // null or undefined
+    if (options.emitKeyChanges === undefined || options.emitKeyChanges === null) {
+      this.emitKeyChanges = cache._emitKeyChanges;
+    } else {
+      this.emitKeyChanges = !!options.emitKeyChanges;
+    }
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -2,6 +2,67 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const InMemoryCache = require('../');
+const EMIT_KEY_CHANGES_ENABLED = { emitKeyChanges: true };
+const EMIT_KEY_CHANGES_DISABLED = { emitKeyChanges: false };
+
+function _putEntryEventTest (cache, entryOptions, expectedResult) {
+  const cacheKey = 'abc';
+  const entryData = 123;
+
+  let putEventEmitted = false;
+
+  cache.on(`put:${cacheKey}`, (data) => {
+    if (entryData === data) {
+      putEventEmitted = true;
+    }
+  });
+
+  cache.put(cacheKey, entryData, entryOptions);
+  expect(putEventEmitted).to.equal(expectedResult);
+}
+
+function _deleteEntryEventTest (cache, entryOptions, expectedResult) {
+  const cacheKey = 'abc';
+  const entryData = 123;
+
+  let deleteEventEmitted = false;
+
+  cache.put(cacheKey, entryData, entryOptions);
+
+  cache.on(`delete:${cacheKey}`, (data) => {
+    if (entryData === data) {
+      deleteEventEmitted = true;
+    }
+  });
+
+  cache.delete(cacheKey);
+  expect(cache.get(cacheKey)).to.equal(undefined);
+  expect(deleteEventEmitted).to.equal(expectedResult);
+}
+
+function _reapEntryEventTest (cache, entryOptions, expectedResult, done) {
+  const cacheKey = 'abc';
+  const entryData = 123;
+
+  let entryRemoved = false;
+
+  let options = Object.assign(entryOptions || {}, { ttl: 100 });
+  cache.put(cacheKey, entryData, options);
+
+  cache.on(`delete:${cacheKey}`, (data) => {
+    if (entryData === data) {
+      entryRemoved = true;
+    }
+  });
+
+  cache.startReaper(200);
+
+  setTimeout(() => {
+    expect(cache.get(cacheKey)).to.equal(undefined);
+    expect(entryRemoved).to.equal(expectedResult);
+    done();
+  }, 400);
+}
 
 describe('InMemoryCache', function () {
   let cache;
@@ -24,7 +85,6 @@ describe('InMemoryCache', function () {
   });
 
   it('should allow cache entry with no TTL', () => {
-    const cache = InMemoryCache.create();
     const cacheKey = 'abc';
 
     cache.put(cacheKey, 123, {
@@ -44,7 +104,6 @@ describe('InMemoryCache', function () {
   });
 
   it('should remove expired entry using manual reaping', (done) => {
-    const cache = InMemoryCache.create();
     const cacheKey = 'abc';
 
     cache.put(cacheKey, 123, {
@@ -73,5 +132,167 @@ describe('InMemoryCache', function () {
       expect(cache.get(cacheKey)).to.equal(undefined);
       done();
     }, 400);
+  });
+
+  context('"emitKeyChanges" cache option is not set (default), entry option is not set', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create();
+    });
+
+    it('should NOT emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, undefined, false);
+    });
+
+    it('should NOT emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, undefined, false);
+    });
+
+    it('should NOT emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, undefined, false, done);
+    });
+  });
+
+  context('"emitKeyChanges" cache option is not set, entry option is set to "true"', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create();
+    });
+
+    it('should emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true);
+    });
+
+    it('should emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true);
+    });
+
+    it('should emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true, done);
+    });
+  });
+
+  context('"emitKeyChanges" cache option is not set, entry option is set to "false"', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create();
+    });
+
+    it('should emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false);
+    });
+
+    it('should emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false);
+    });
+
+    it('should emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false, done);
+    });
+  });
+
+  context('"emitKeyChanges" cache option is set to "true", entry option is not set', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create(EMIT_KEY_CHANGES_ENABLED);
+    });
+
+    it('should emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, undefined, true);
+    });
+
+    it('should emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, undefined, true);
+    });
+
+    it('should emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, undefined, true, done);
+    });
+  });
+
+  context('"emitKeyChanges" cache option is set to "true", entry option is set to "true"', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create(EMIT_KEY_CHANGES_ENABLED);
+    });
+
+    it('should emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true);
+    });
+
+    it('should emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true);
+    });
+
+    it('should emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true, done);
+    });
+  });
+
+  context('"emitKeyChanges" cache option is set to "true", entry option is set to "false"', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create(EMIT_KEY_CHANGES_ENABLED);
+    });
+
+    it('should override cache option and NOT emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false);
+    });
+
+    it('should override cache option and NOT emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false);
+    });
+
+    it('should override cache option and NOT emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false, done);
+    });
+  });
+
+  context('"emitKeyChanges" cache option is set to "false", entry option is not set', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create(EMIT_KEY_CHANGES_DISABLED);
+    });
+
+    it('should NOT emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, undefined, false);
+    });
+
+    it('should NOT emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, undefined, false);
+    });
+
+    it('should NOT emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, undefined, false, done);
+    });
+  });
+
+  context('"emitKeyChanges" cache option is set to "false", entry option is set to "true"', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create(EMIT_KEY_CHANGES_DISABLED);
+    });
+
+    it('should override cache option and emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true);
+    });
+
+    it('should override cache option and emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true);
+    });
+
+    it('should override cache option and emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, EMIT_KEY_CHANGES_ENABLED, true, done);
+    });
+  });
+
+  context('"emitKeyChanges" cache option is set to "false", entry option is set to "false"', () => {
+    beforeEach(() => {
+      cache = InMemoryCache.create(EMIT_KEY_CHANGES_DISABLED);
+    });
+
+    it('should NOT emit an event when an entry is added', () => {
+      _putEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false);
+    });
+
+    it('should NOT emit an event when an entry is deleted', () => {
+      _deleteEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false);
+    });
+
+    it('should NOT emit an event when an entry is reaped', (done) => {
+      _reapEntryEventTest(cache, EMIT_KEY_CHANGES_DISABLED, false, done);
+    });
   });
 });
